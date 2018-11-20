5528b8eb743544249e63881692134e398d53341a
cameronconradt <cameronconradt@gmail.com>
2018-11-20 01:17:59 -0700

    streamTrainCards only filtered by gameID to accomodate for returning to deck

--- src/main/java/com/wwttr/database/DatabaseFacade.java
+++ src/main/java/com/wwttr/database/DatabaseFacade.java
@@ -635,7 +635,7 @@ public class DatabaseFacade {
     String gameId = getPlayer(playerId).getGameId();
     return trainCardQueue
         .subscribe()
-        .filter((TrainCard tc) -> tc.getGameId().equals(gameId) && !tc.getState().equals(TrainCard.State.HIDDEN) );
+        .filter((TrainCard tc) -> tc.getGameId().equals(gameId) );
   }
 
   public Stream<DeckStats> streamDeckStats(String gameId){
