79157f902bbd09ec6468c5b64af1ca212b10de17
cameronconradt <cameronconradt@gmail.com>
2018-12-08 16:31:05 -0700

    finish implementation of GameDaos, need to test

--- src/main/java/com/wwttr/dao/GameDAO.java
+++ src/main/java/com/wwttr/dao/GameDAO.java
@@ -4,21 +4,40 @@ import com.wwttr.database.DatabaseFacade;
 
 public abstract class GameDAO implements DAO {
 
-  String connectionString;
+  public final String connectionString;
+
+  public GameDAO(){throw new IllegalArgumentException("connectionstring must be given");}
 
   public GameDAO(String connectionString) {
     this.connectionString = connectionString;
   }
 
   @Override
-  public void save(DatabaseFacade facade){
-
+  public final void save(DatabaseFacade facade){
+    saveToPersistance(facade);
   }
 
   @Override
-  public void load(DatabaseFacade facade){
+  public final void load(DatabaseFacade facade){
+    DatabaseFacade persistantFacade = loadFromPersistance();
+    facade.setGames(persistantFacade.getGames());
+    facade.setGameActions(persistantFacade.getGameActions());
+    facade.setGameStream(persistantFacade.getGameStream());
+    facade.setRoutes(persistantFacade.getRoutes());
+    facade.setRouteQueue(persistantFacade.getRouteQueue());
+    facade.setTrainCards(persistantFacade.getTrainCards());
+    facade.setTrainCardQueue(persistantFacade.getTrainCardQueue());
+    facade.setDestinationCards(persistantFacade.getDestinationCards());
+    facade.setDestinationCardQueue(persistantFacade.getDestinationCardQueue());
+    facade.setDeckStatsCommandQueue(persistantFacade.getDeckStatsCommandQueue());
+    facade.setHistoryQueue(persistantFacade.getHistoryQueue());
+    facade.setMessages(persistantFacade.getMessages());
+    facade.setMessageQueue(persistantFacade.getMessageQueue());
+    facade.setPlayers(persistantFacade.getPlayers());
+    facade.setPlayerStatsQueue(persistantFacade.getPlayerStatsQueue());
 
   }
 
-  public abstract void writeToPersistance();
+  public abstract DatabaseFacade loadFromPersistance();
+  public abstract void saveToPersistance(DatabaseFacade facade);
 }
---
+++ src/main/java/com/wwttr/dao/GameDAONoSQL.java
@@ -0,0 +1,54 @@
+package com.wwttr.dao;
+
+import android.provider.ContactsContract;
+
+import com.wwttr.database.DatabaseFacade;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamException;
+
+public class GameDAONoSQL extends GameDAO {
+
+  @Override
+  public DatabaseFacade loadFromPersistance() {
+    try {
+      FileInputStream fileInputStream = new FileInputStream(connectionString);
+      ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+      return (DatabaseFacade) objectInputStream.readObject();
+    }
+    catch (FileNotFoundException e){
+      throw new IllegalArgumentException("File not found");
+    }
+    catch (IOException e){
+      throw new IllegalArgumentException("IOException");
+    }
+    catch (ClassNotFoundException e){
+      throw new IllegalArgumentException("Class not Found");
+    }
+
+  }
+
+  @Override
+  public void saveToPersistance(DatabaseFacade facade) {
+    try {
+      FileOutputStream fileOutputStream = new FileOutputStream(connectionString, true);
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+      objectOutputStream.writeObject(facade);
+      objectOutputStream.close();
+      fileOutputStream.close();
+    }
+    catch (FileNotFoundException e){
+      throw new IllegalArgumentException("File not found");
+    }
+    catch (IOException e){
+      throw new IllegalArgumentException("IOException");
+    }
+  }
+}
