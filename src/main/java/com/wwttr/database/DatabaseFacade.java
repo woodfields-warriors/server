a0d1b180f40faaf85157e622cf36fe9db2d6ee55
Hunter Rasmussen Lubuntu <hunterhrasmussen@gmail.com>
2018-12-14 17:19:14 -0700

    changed to transient

--- src/main/java/com/wwttr/database/DatabaseFacade.java
+++ src/main/java/com/wwttr/database/DatabaseFacade.java
@@ -916,7 +916,7 @@ public void execute(Delta delta) {
   String serviceName = getServiceFromMessage(request);
   System.out.println("Executing the following command loaded from deltaDao: "
                     +serviceName +"."+methodName);
-  Handler handler = Handler.getInstance();
+  transient Handler handler = Handler.getInstance();
   try {
     handler.handleFromStrings(request, delta.getId(), methodName, serviceName);
   }
